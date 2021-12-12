45bb7a6da10165d927371ae9fee6b24a12d77bce
CosmoboyMe <2014failz2014@gmail.com>
2021-12-12 17:46:14 +0100

    feat: add modal

--- src/components/Organisms/PokemonCards/index.tsx
+++ src/components/Organisms/PokemonCards/index.tsx
@@ -1,27 +1,50 @@
 import './PokemonCards.scss'
-import { FC, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { FC, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../../../hooks'
-import { pokemonsSelector } from '../../../selectors'
 
+import { loadPokemonCards } from '../../../store/Slices/pokemonsSlice'
 import {
-    loadPokemonCards,
     setSelectedPokemonId,
-} from '../../../store/Slices/pokemonsSlice'
+    loadPokemonCard,
+} from '../../../store/Slices/pokemonSlice'
 
+import { pokemonSelector, pokemonsSelector } from '../../../selectors'
+import { SCREENS } from '../../../routes/endpoints'
+
+import { Loader } from '../../Atoms/Loader'
 import { PokemonCard } from '../../Molecules/PokemonCard'
 import { Modal } from '../Modal'
-import { Loader } from '../../Atoms/Loader'
-import { SCREENS } from '../../../routes/endpoints'
+import { ModalPokemonContent } from '../ModalPokemonContent'
 
 const PokemonCards: FC = () => {
     const { pokemons, fetching } = useAppSelector(pokemonsSelector)
+    const {
+        selectedPokemon,
+        selectedPokemonId,
+        fetching: isPokemonFetcing,
+    } = useAppSelector(pokemonSelector)
+    const [isOpen, setIsOpen] = useState(false)
+    const navigate = useNavigate()
     const dispatch = useAppDispatch()
+
+    const handlerClickLi = (id: string) => {
+        dispatch(setSelectedPokemonId(id))
+        dispatch(loadPokemonCard(id))
+        setIsOpen(true)
+    }
+
+    const handlerCloseModal = () => {
+        setIsOpen(false)
+    }
+
+    const handlerClickMoveToPokemonPage = () => {
+        navigate(`/pokemon/${selectedPokemonId}`)
+    }
 
     useEffect(() => {
         dispatch(loadPokemonCards())
     }, [dispatch])
-
     return (
         <div className="pokemon-cards">
             {fetching ? (
@@ -35,25 +58,37 @@ const PokemonCards: FC = () => {
                             ({ id, name, artist, images: { small } }) => {
                                 return (
                                     <li
-                                        onClick={() =>
-                                            dispatch(setSelectedPokemonId(id))
-                                        }
+                                        onClick={() => handlerClickLi(id)}
                                         key={id}
                                         className="pokemon-cards__item"
                                     >
-                                        <Link to={SCREENS.SCREEN_POKEMON}>
-                                            <PokemonCard
-                                                name={name}
-                                                artist={artist}
-                                                imgSrc={small}
-                                            />
-                                        </Link>
+                                        <PokemonCard
+                                            name={name}
+                                            artist={artist}
+                                            imgSrc={small}
+                                        />
                                     </li>
                                 )
                             }
                         )}
                     </ul>
-                    {/* <Modal /> */}
+                    <Modal open={isOpen} closeModal={handlerCloseModal}>
+                        {selectedPokemon &&
+                            (isPokemonFetcing ? (
+                                <div className="pokemon-cards__loader">
+                                    <Loader />
+                                </div>
+                            ) : (
+                                <ModalPokemonContent
+                                    name={selectedPokemon.name}
+                                    imgSrc={selectedPokemon.images.small}
+                                    onClose={handlerCloseModal}
+                                    onMoreDetails={
+                                        handlerClickMoveToPokemonPage
+                                    }
+                                />
+                            ))}
+                    </Modal>
                 </>
             )}
         </div>
